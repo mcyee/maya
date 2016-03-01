fa80ebc73c274376964b07df1fea75ec2725f53d
mcyee <mingcee@skysketches.com>
2016-02-29 23:36:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Maya
+
+Maya is a Slack bot user. She doesn't really do anything yet.
+
+## Dependencies
+
+Please install the [Slack client library for node.js](https://github.com/slackhq/node-slack-client). You might want to install [config.json](https://github.com/bulyshko/config.json) as well.
---
+++ maya.js
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * MAYA
+ * Author: Ming-Cee Yee // skysketches.com
+ * Date: Feb 2016
+ * Description: Bot that handles team events
+ ******************************************************************************/
+
+var config = require('config.json');
+
+// Create client
+var RtmClient = require('@slack/client').RtmClient;
+var token = process.env.SLACK_API_TOKEN || config.token;
+var rtm = new RtmClient(token, {logLevel: 'debug'});
+
+// Get stuff
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
+
+var channel;
+
+// Listen to `message` events
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+	channel = message.channel;
+});
+
+rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function() {
+	if (channel) {
+		rtm.sendMessage('Hello world!', channel, function messageSent() {
+			console.log("Sent message to a channel");
+		});
+	}
+});
+
+rtm.start();
